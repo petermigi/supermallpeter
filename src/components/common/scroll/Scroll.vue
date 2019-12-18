de705540dd18fa53c90f1bc4ea33078319bd2abf
peterliu_glb <peterliu_glb@163.com>
2019-12-18 08:43:12 +0800

    day11 04-(掌握)刷新频繁的防抖函数处理

---
+++ src/components/common/scroll/Scroll.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="wrapper" ref="wrapper">
+    <div class="content">
+      <slot></slot>
+    </div>
+  </div>
+
+</template>
+
+<script>
+  import BScroll from 'better-scroll'
+
+  export default {
+    name: "Scroll",
+    props: {
+      probeType: {
+        type: Number,
+        default: 0
+      }
+
+    },
+    data(){
+      return {
+        scroll: null
+      }
+    },
+    mounted() {
+      // 1.创建BScroll对象
+      this.scroll = new BScroll(this.$refs.wrapper,{
+        click: true,
+        probeType: this.probeType,
+        pullUpLoad: this.pullUpLoad
+      })
+
+      // 2.监听滚动的位置
+      this.scroll.on('scroll',(position) => {
+        this.$emit('scroll', position)
+      })
+
+
+    },
+    methods: {
+      scrollTo(x, y, time=300){
+        this.scroll && this.scroll.scrollTo(x, y, time)
+      },
+      finishPullUp(){
+        this.scroll && this.scroll.finishPullUp()
+      },
+      refresh(){
+        console.log("----");
+        this.scroll && this.scroll.refresh()
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
