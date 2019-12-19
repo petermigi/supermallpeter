ecfd75b35151465aabcf9cd5899021d815656a78
peterliu_glb <peterliu_glb@163.com>
2019-12-19 11:20:58 +0800

    09-(掌握)跳转到详情页并且携带iid

--- src/components/common/scroll/Scroll.vue
+++ src/components/common/scroll/Scroll.vue
@@ -61,6 +61,9 @@
       },
       finishPullUp(){
         this.scroll && this.scroll.finishPullUp()
+      },
+      getScrollY() {
+        return this.scroll ? this.scroll.y : 0
       }
     }
   }
